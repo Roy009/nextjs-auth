706b11dfcd956a1493a652e5bd95c25e122e355f
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-07-16 23:32:47 +0530

    user profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,8 @@
 export default function UserProfile({params} : any){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl">Profile Page {params.id}</h1>
+            <h1 className="text-4xl">Profile Page</h1>
+            <p>{params.id}</p>
         </div>
     )
 }
